18e5f0163bc68c297c511f697d134191cc293d8f
luyoyo <liulurose@sina.com>
2020-05-31 17:53:14 +0800

    最长回文字符串

--- string/Longest_Palindromic_Substring.php
+++ string/Longest_Palindromic_Substring.php
@@ -30,6 +30,38 @@ function getCenterLen($s, $left, $right) {
     return $right - $left - 1;
 }
 
+
+/**
+ * 方法2:
+ * https://github.com/labuladong/fucking-algorithm/blob/master/%E9%AB%98%E9%A2%91%E9%9D%A2%E8%AF%95%E7%B3%BB%E5%88%97/%E6%9C%80%E9%95%BF%E5%9B%9E%E6%96%87%E5%AD%90%E4%B8%B2.md
+ */ 
+
+function palindrome($s, $l, $r) {
+    while ($l >= 0 && $r < strlen($s) && $s[$l] == $s[$r]) {
+        $l--;
+        $r++;
+    }
+    return substr($s, $l + 1, $r - $l - 1);
+}
+
+function longestPalindrome2($s) {
+    $len = strlen($s);
+    if ($len <= 1) {
+        return $s;
+    }
+    $res = '';
+    for ($i = 0; $i < $len; $i++) {
+        $s1 = palindrome($s, $i, $i);
+        $s2 = palindrome($s, $i, $i + 1);
+
+        $res = strlen($s1) > strlen($res) ? $s1 : $res;
+        $res = strlen($s2) > strlen($res) ? $s2 : $res;
+    }
+    return $res;
+}
+
+
+
 $s = "cbabcad";
-$res = longestPalindrome($s);
+$res = longestPalindrome2($s);
 print_r($res);
